e2fdecabd15747858cd8db0d11759bfb836b5971
diana <dimiquigo@gmail.com>
2018-09-09 04:17:05 -0500

    se hace hace diseño de la auto institucional con preguntas

--- vistas/modulos/autoinstitucional.php
+++ vistas/modulos/autoinstitucional.php
@@ -1,1 +1,98 @@
-ggggggg
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        General Form Elements
+        <small>Preview</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Forms</a></li>
+        <li class="active">General Elements</li>
+    </ol>
+</section>
+
+
+
+<!-- Main content -->
+<section class="content">
+    <div class="row">
+  
+        <!-- right column -->
+        <div class="col-md-6">
+            <!-- general form elements -->
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Autoevaluacion Institucional</h3>
+                </div>
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form role="form">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">
+                                ¿Ha participado o liderado algún estudio de demanda de los perfiles profesionales y ocupacionales que consideren los requerimientos locales, regionales e internacionales en los últimos 10 años?
+                            </label>
+                            <select class="form-control">
+                                <option value="-1">seleccione</option>
+                                <option value="1">si</option>
+                                <option value="2">no</option>
+                                <option value="3">no se</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">
+                            ¿En los últimos 5 años, ha actualizado su programa de curso con alguna innovación pedagógica?
+                            </label>
+                            <select class="form-control">
+                                <option value="-1">seleccione</option>
+                                <option value="1">si</option>
+                                <option value="2">no</option>
+                                <option value="3">no se</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">
+                                ¿Existen lineamientos para desarrollar los programas analíticos a nivel de pregrado, grado y postgrado?
+                            </label>
+                            <select class="form-control">
+                                <option value="-1">seleccione</option>
+                                <option value="1">si</option>
+                                <option value="2">no</option>
+                                <option value="3">no se</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">¿Existen políticas institucionales para la formación integral del curriculum?</label>
+                            <select class="form-control">
+                                <option value="-1">seleccione</option>
+                                <option value="1">si</option>
+                                <option value="2">no</option>
+                                <option value="3">no se</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">¿Considera que la oferta académica es flexible y se adapta a los cambios que requiere la sociedad?</label>
+                            <select class="form-control">
+                                <option value="-1">seleccione</option>
+                                <option value="1">si</option>
+                                <option value="2">no</option>
+                                <option value="3">no se</option>
+                            </select>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <!-- /.box -->    
+
+
+        </div>
+        <!--/.col (right) -->
+    </div>
+    <!-- /.row -->
+</section>
+<!-- /.content -->
